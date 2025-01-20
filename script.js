273854e7c919847332a4cf4cd963bc071f82548d
Derick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2025-01-20 19:43:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.getElementById('showTemas').addEventListener("click", function() {
+    document.querySelector(".temas").classList.toggle("open");
+});
+
 function baixarCurriculo() {
     const link = document.createElement('a');
     link.href = 'curriculo.pdf'; 
@@ -16,34 +20,58 @@ function fecharModal(){
 }
 
 function changeToOrange(){
+    const nome = document.getElementById('nome');
+    const copy = document.getElementById('copy');
+    const marketing = document.getElementById('marketing');
     const rede = document.getElementsByClassName('rede');
     for (let i=0; i < rede.length; i++){
         rede[i].style.background = "#fff";
     }
-    document.body.style.backgroundImage = 'linear-gradient(to right, red, orange)';
+    nome.style.color = "#fff";
+    copy.style.color = "#ccc";
+    marketing.style.color = "#fff";
+    document.body.style.backgroundImage = 'linear-gradient(to right, red, rgb(255, 123, 0))';
 }
 
 function changeToBlue(){
+    const nome = document.getElementById('nome');
+    const copy = document.getElementById('copy');
+    const marketing = document.getElementById('marketing');
     const rede = document.getElementsByClassName('rede');
     for (let i=0; i < rede.length; i++){
         rede[i].style.background = "#fff";
     }
+    nome.style.color = "#fff";
+    copy.style.color = "#ccc";
+    marketing.style.color = "#fff";
     document.body.style.backgroundImage = 'linear-gradient(to right, rgb(0, 49, 211), rgb(3, 210, 247))';
 }
 
 function changeToGreen(){
+    const nome = document.getElementById('nome');
+    const copy = document.getElementById('copy');
+    const marketing = document.getElementById('marketing');
     const rede = document.getElementsByClassName('rede');
     for (let i=0; i < rede.length; i++){
         rede[i].style.background = "#fff";
     }
+    nome.style.color = "#fff";
+    copy.style.color = "#fff";
+    marketing.style.color = "#000";
     document.body.style.backgroundImage = 'linear-gradient(to right, green, lightgreen)';
 }
 
 function changeToBlack(){
+    const nome = document.getElementById('nome');
+    const copy = document.getElementById('copy');
+    const marketing = document.getElementById('marketing');
     const rede = document.getElementsByClassName('rede');
     for (let i=0; i < rede.length; i++){
         rede[i].style.background = "#ccc";
     }
+    nome.style.color = "#fff";
+    copy.style.color = "#ccc";
+    marketing.style.color = "#fff";
     document.body.style.background = "#333";
 }
 
